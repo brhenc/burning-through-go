0c007d6cf24bac77bfd746abc76e7359c4c364ea
bhenc <bhenc@localhost.localdomain>
2017-10-25 21:33:36 +0200

    Every new language is just C with messed up syntax
    Lobotomy would be more comfortable. Touted to be easy
    to graps for people who used C... why not use C ?
    
    Have to find gdb, valgrind, build environment, include
    paths, packaging rules, sane libraries for this clustered
    mess. Not enjoying you golang at all.

---
+++ helloworld/helloworld.go
@@ -0,0 +1,9 @@
+package main // What the hell is this shit? 
+
+import "fmt" /* python style imports, but requires "" */
+//also retarted, fmt stands for formatted I guess, I/O being implied
+//I/O to what exactly? Anywhere, here's the link https://golang.org/pkg/fmt/
+func main() {
+	fmt.Println("Hello, World!") //why library author, did you capitalize Print ? To be special ? it's print() or print "" in python, printf in C, printf in Bash, print in Ruby
+/* */
+}
