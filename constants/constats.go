796c2299aa71b8c0a7a74e5b16f6f42d4b6d7e35
Bruno Henc <hencb@protonmail.ch>
2017-10-27 16:56:28 +0200

    commiting interrim progress

--- constants/constats.go
+++ constants/constats.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-import "math" 
+import "math"
 
 const s string = "constant"
 
---
+++ structs/structs.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type person struct {
+    name string
+    age int
+}
+
+func main() {
+
+    fmt.Println(person{"Bob", 20})
+
+    fmt.Println(person{name: "alice", age: 30})
+
+
+}
